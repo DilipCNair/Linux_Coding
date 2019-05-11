9a1a90ceadea15d85f2f584991175dc1d03e4553
DilipCNair <dilipn6@gmail.com>
2019-05-11 14:33:50 +0530

    test

--- Java/Factorial/Factorial.java
+++ Java/Factorial/Factorial.java
@@ -1,11 +1,12 @@
 public class Factorial
 {
 	public static void main(String[] args)
-	{	final int NUM_FACTS = 100;
+	{	
+		final int NUM_FACTS = 100;
 		for(int i = 0; i < NUM_FACTS; i++)
 			System.out.println( i + "! is " + factorial(i));
 	}
-
+	
 	public static int factorial(int n)
 	{		
 		int result = 1;
