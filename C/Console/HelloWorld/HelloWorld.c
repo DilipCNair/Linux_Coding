79f0f7f3185aa81997c62097a460fb3d5dcdc332
DilipCNair <dilipn6@gmail.com>
2020-04-11 11:16:48 +0530

    Test

--- C/Console/HelloWorld/HelloWorld.c
+++ C/Console/HelloWorld/HelloWorld.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+  //Tetsing
   printf("Hello World!\n");
   return 0;
 }
--- CPP/Console/HelloWorld/HelloWorld.cpp
+++ CPP/Console/HelloWorld/HelloWorld.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main()
 {
-    //Checking
+    //Checking 1
     cout << "Hello, World!\n";
+    //Checking 2
     return 0;
 }
