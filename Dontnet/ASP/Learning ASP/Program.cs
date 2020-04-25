dd5ebdfaed386f45414422e5313e96061f2ff637
DilipCNair <dilipn6@gmail.com>
2020-04-25 12:00:37 +0530

    over

--- Dontnet/ASP/Learning ASP/Program.cs
+++ Dontnet/ASP/Learning ASP/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MySql.Data.EntityFrameworkCore;
 
 namespace Learning_ASP
 {
