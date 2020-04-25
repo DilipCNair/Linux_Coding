c943f86a3fa401aca21000cce4d9e44e8559756d
DilipCNair <dilipn6@gmail.com>
2020-04-25 20:03:18 +0530

    over

---
+++ Dotnet/Console/Learning_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Learning_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,num = 10,sum=50;
+            for(i=0;i<num;i++)
+             sum=sum+i;           
+            Console.WriteLine("Hello World!"+sum);
+        }
+    }
+}
