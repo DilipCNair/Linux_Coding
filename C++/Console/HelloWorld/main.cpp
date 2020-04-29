9be26f38927a164007f36cfc9df445d85b8bc1aa
DilipCNair <dilipn6@gmail.com>
2020-04-29 14:59:50 +0530

    over

--- C++/Console/HelloWorld/main.cpp
+++ C++/Console/HelloWorld/main.cpp
@@ -5,5 +5,10 @@ int main()
     int i,j;
     i=10;
     j=15;
+    int k;
     cout<<"Hello World!\n"<<i<<" "<<j<<endl;
+    cout<<"Enter Some integer : ";
+    cin>>k;
+    cout<<"You have entered : "<<k<<endl;
+
 }
