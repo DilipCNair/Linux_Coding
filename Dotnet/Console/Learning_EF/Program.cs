8d4f18e44aa6aa4ead69df611d9e31635bb70e55
DilipCNair <dilipn6@gmail.com>
2020-08-12 10:40:06 +0530

    Over

---
+++ Dotnet/Console/Learning_EF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning_EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
