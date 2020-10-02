e3705545bacf24b0377fd2e2e41e0fc848d08185
DilipCNair <dilipn6@gmail.com>
2020-10-02 18:16:05 +0530

    Over

---
+++ Python/Pycharm/main.py
@@ -0,0 +1,14 @@
+
+# name = input("Enter you Name : ")
+# print("Hi " + name)
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+# Sum = num1 + num2
+# print("The sum is : " + str(Sum))
+
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+Sum = num1 + num2
+print("The sum is : " + str(Sum))
+
