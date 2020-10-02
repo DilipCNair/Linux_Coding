8c490d82b1412687fd51afcc35142fd1ef670585
DilipCNair <dilipn6@gmail.com>
2020-10-02 19:18:50 +0530

    Over

--- Python/Reference/Addition.py
+++ Python/Reference/Addition.py
@@ -1,7 +1,4 @@
 
-# name = input("Enter you Name : ")
-# print("Hi " + name)
-
 # num1 = input("Enter first number : ")
 # num2 = input("Enter second number : ")
 # Sum = num1 + num2
@@ -11,5 +8,3 @@ num1 = float(input("Enter first number : "))
 num2 = float(input("Enter second number : "))
 Sum = num1 + num2
 print("The sum is : " + str(Sum))
-
-#Test
