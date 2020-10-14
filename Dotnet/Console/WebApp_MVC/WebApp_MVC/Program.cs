d8b584845e55939f17beed5304aa69fa10d97887
DilipCNair <dilipn6@gmail.com>
2020-10-14 19:34:47 +0530

    Finish

--- Dotnet/Console/WebApp_MVC/WebApp_MVC/Program.cs
+++ Dotnet/Console/WebApp_MVC/WebApp_MVC/Program.cs
@@ -15,9 +15,10 @@ namespace WebApp_MVC
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        
     }
 }
