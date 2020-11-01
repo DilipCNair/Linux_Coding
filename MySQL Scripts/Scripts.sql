d5a8e8f2e278a58e0cd45cca44917a82c64afe3d
Dilip C Nair <dilipn6@gmail.com>
2020-11-01 10:59:51 +0530

    Finish

--- MySQL Scripts/Scripts.sql
+++ MySQL Scripts/Scripts.sql
@@ -1,20 +1,25 @@
 
 /*
-mysql runs at 127.0.0.1:3306 by default
+To install mysql run following command
+    sudo apt install mysql-server mysql-client
+For initial configuration of mysql run following command
+    sudo mysql_secure_installation
 service mysql status/start/stop/restart are basic commands
-current admin account username is "root" and password is "Dilip@1692"
-mysql -u root -p and enter password to start mysql shell
+Mysql runs at 127.0.0.1:3306 by default
+Current admin account username is "root" and password is "Mysql@1692"
+sudo mysql command to login using auth-socket
+mysql -u root -p and enter password to start mysql shell/client normally
+auth_socket plugin needs to be changed for normal login - research on that
 */
 
 Use mysql;
-CREATE USER 'YOUR_SYSTEM_USER'@'localhost' IDENTIFIED BY 'new-password';
-UPDATE user SET plugin='mysql_native_password' WHERE User='YOUR_SYSTEM_USER';
-GRANT ALL PRIVILEGES ON *.* TO 'YOUR_SYSTEM_USER'@'localhost';
-UPDATE user SET plugin='auth_socket' WHERE User='YOUR_SYSTEM_USER';
-FLUSH PRIVILEGES;
 SELECT User, Host, plugin FROM mysql.user;
+UPDATE user SET plugin='mysql_native_password' WHERE User='YOUR_SYSTEM_USER';
+UPDATE user SET plugin='auth_socket' WHERE User='YOUR_SYSTEM_USER';
 ALTER USER 'YOUR_SYSTEM_USER'@'localhost' IDENTIFIED WITH mysql_native_password BY 'new-password';
-
+CREATE USER 'YOUR_SYSTEM_USER'@'localhost' IDENTIFIED BY 'new-password';
+GRANT ALL PRIVILEGES ON *.* TO 'YOUR_SYSTEM_USER'@'localhost';                                                    
+FLUSH PRIVILEGES;
 
 
 CREATE DATABASE dotnet;
